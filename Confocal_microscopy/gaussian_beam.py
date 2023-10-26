5bdaf9e4080d36bfa3fec1773adcf3d63616e33a
andreabassi78 <andreabassi@polimi.it>
2023-10-26 22:48:38 +0200

    added confocal with pinhole

--- Confocal_microscopy/gaussian_beam.py
+++ Confocal_microscopy/gaussian_beam.py
@@ -1,52 +1,77 @@
-
-
 import numpy as np
 from numpy.fft import fft2, ifft2, ifftshift, fftshift
 import matplotlib.pyplot as plt
+
+def convolve(X,Y):
+    FX = fftshift(fft2(X))
+    FY = fftshift(fft2(Y))
+    return ifftshift(ifft2(FX*FY))
 
 um = 1.0
 mm = 1000.0
 pi = np.pi
 
-Npixels = 256 # Pixels in x,y and number of planes z
+Npixels = 256 # Pixels in x and z
 wavelength = 0.500*um 
 n = 1
-w0 = 0.29 * um
+w0 = 0.199 * um
+zr = pi * w0**2 *n / wavelength
 
-zr = np.pi * w0**2 *n / wavelength
-print(zr)
 # define the space at the focus plane
-a = 8 * um 
-b = 8 * um
+a = 2 * um 
+b = 3 * um
 rho = np.linspace(-a, +a, Npixels)
 z = np.linspace(-b, +b, Npixels)
 Rho, Z = np.meshgrid(rho,z)
 
-w= w0 * np.sqrt(1+(Z/zr)**2)
+w = w0 * np.sqrt(1+(Z/zr)**2)
+I = np.exp(-2*Rho**2/w**2) * (w0/w)**2 # Intensity of a Gaussian beam that simulates the illumination and (ideal) detection PSF
 
-I = np.exp(-2*Rho**2/w**2) * (w0/w)**2
+pinhole = np.zeros_like(I)
+r = 22.5/40 * um # radius of the pinhole at the object 
+ph = np.abs(rho) < r
+pinhole[Npixels//2, ph] = 1
 
-PSF = I
+PSF_illumination = I
+PSF_detection = np.abs(convolve(I,pinhole)) 
+PSF_conf = PSF_illumination * PSF_detection
 
-plt.gray()
 fig0, ax0 = plt.subplots()
-ax0.imshow(PSF, extent = [np.amin(rho),np.amax(rho),np.amin(z),np.amax(z)])
+ax0.imshow(PSF_illumination, extent = [np.amin(rho),np.amax(rho),np.amin(z),np.amax(z)])
 plt.xlabel('x (um)')
 plt.ylabel('z (um)')
-plt.title(f'|PSF(x,z|')
+plt.title(f'PSF_illumination(x,z)')
+
+fig0, ax0 = plt.subplots()
+ax0.imshow(pinhole, extent = [np.amin(rho),np.amax(rho),np.amin(z),np.amax(z)])
+plt.xlabel('x (um)')
+plt.ylabel('z (um)')
+plt.title(f'pinhole')
+
+fig0, ax0 = plt.subplots()
+ax0.imshow(PSF_detection, extent = [np.amin(rho),np.amax(rho),np.amin(z),np.amax(z)])
+plt.xlabel('x (um)')
+plt.ylabel('z (um)')
+plt.title(f'PSF_detection(x,z)')
+
+fig0, ax0 = plt.subplots()
+ax0.imshow(PSF_conf, extent = [np.amin(rho),np.amax(rho),np.amin(z),np.amax(z)])
+plt.xlabel('x (um)')
+plt.ylabel('z (um)')
+plt.title(f'PSF_confocal(x,z)')
 
 dkx = 1/2/np.amax(rho)
 dkz = 1/2/np.amax(z)
 kx = np.linspace (- dkx*Npixels/2, + dkx*Npixels/2, Npixels)
 kz = np.linspace (- dkz*Npixels/2, + dkz*Npixels/2, Npixels)
 
-OTF = fftshift(fft2(PSF)) # Optical Transfer Function
+# OTF = fftshift(fft2(PSF_conf)) # Optical Transfer Function
+# fig0, ax0 = plt.subplots()
+# ax0.imshow(np.abs(OTF), extent = [np.amin(kx),np.amax(kx),np.amin(kz),np.amax(kz)])
+# plt.xlabel('kx (1/um)')
+# plt.ylabel('kz (1/um)')
+# plt.title(f'|OTF(kx,kz|')
+# plt.gray()
 
-fig0, ax0 = plt.subplots()
-ax0.imshow(np.log10(np.abs(OTF)+10), extent = [np.amin(kx),np.amax(kx),np.amin(kz),np.amax(kz)])
-plt.xlabel('kx (1/um)')
-plt.ylabel('kz (1/um)')
-plt.title(f'log|OTF(kx,kz|')
-plt.gray()
 plt.show()
 
